b9f551ffcb010d5ce9a433b2cf6d2ef76d176e1d
jonathan <jonathan.scheffel@st.oth-regensburg.de>
2022-07-01 09:31:18 -0400

    Added Charts for admin analytics dashboard and user detail view.

--- waterfall-front-end/src/components/UserList.svelte
+++ waterfall-front-end/src/components/UserList.svelte
@@ -1,6 +1,7 @@
 <script>
     import {createEventDispatcher, getContext, onMount} from "svelte";
     import {push} from "svelte-spa-router";
+    import Chart from 'svelte-frappe-charts';
 
     const dispatch = createEventDispatcher();
 
@@ -12,11 +13,53 @@
     let appliedFilter = "";
     let userSelect;
 
+    let analyticsAdmin = {num: 0, cumPwdLength: 0};
+    let analyticsUser = {num: 0, cumPwdLength: 0};
+
+    let privilegeDistributionData;
+    let averagePwdLength;
+
     const waterfallService = getContext("WaterfallService");
+
+    function analyseData() {
+        analyticsAdmin = {num: 0, cumPwdLength: 0};
+        analyticsUser = {num: 0, cumPwdLength: 0};
+
+        userList.forEach(user => {
+            if (user.scope === "admin") {
+                analyticsAdmin.num += 1;
+                analyticsAdmin.cumPwdLength += user.passwordLength;
+            } else {
+                analyticsUser.num += 1;
+                analyticsUser.cumPwdLength += user.passwordLength;
+            }
+        });
+        privilegeDistributionData = {
+            labels: ["Admin", "User"],
+            datasets: [
+                {
+                    values: [analyticsAdmin.num, analyticsUser.num],
+                }
+            ]
+        };
+        const averageAdminPwdLength = analyticsAdmin.cumPwdLength / analyticsAdmin.num;
+        const averageUserPwdLength = analyticsUser.cumPwdLength / analyticsUser.num;
+        const averageAllPwdLength = (analyticsAdmin.cumPwdLength + analyticsUser.cumPwdLength) / (analyticsAdmin.num + analyticsUser.num);
+
+        averagePwdLength = {
+            labels: ["All","Admin", "User"],
+            datasets: [
+                {
+                    values: [averageAllPwdLength, averageAdminPwdLength, averageUserPwdLength],
+                }
+            ]
+        };
+    }
 
     onMount(async () => {
             userList = await waterfallService.getUserAnalyticsList();
             filterApplied();
+            analyseData();
         }
     );
 
@@ -41,60 +84,76 @@
 </script>
 
 <div class="columns is-centered">
-    <div class="box column is-6 m-2">
-        {#if Object.keys(userFilterList).length === 0}
-            <p><i class="far fa-folder-open"/> <span>No users found for these filters</span></p>
-        {:else}
-            <div class="table-container">
-                <table class="table is-fullwidth">
-                    <thead>
-                    <th>Name</th>
-                    <th>Scope</th>
-                    </thead>
-                    <tbody>
-                    {#each userFilterList as user}
-                        <tr on:click={clicked(user)}>
-                            <td>
-                                {user.name}
-                            </td>
-                            <td>
-                                {#if user.scope === "admin"}
-                                    <span class="tag is-danger is-uppercase">{user.scope}</span>
-                                {:else }
-                                    <span class="tag is-primary is-uppercase">{user.scope}</span>
-                                {/if}
-                            </td>
-                        </tr>
-                    {/each}
-                    </tbody>
-                </table>
-            </div>
-        {/if}
-    </div>
 
-    <div class="box column is-2 m-2">
-        <div class="level-item m-3">
-            <p class="subtitle is-size-4 is-italic">Filter users</p>
-        </div>
-        <div class="level-item m-2">
-            <p class="subtitle is-5">Scope</p>
-        </div>
-        <div class="level-item m-2">
-            <div class="control has-icons-left">
-                <div class="select is-primary">
-                    <select bind:this={userSelect}>
-                        {#each userSelectPossibilities as possibility}
-                            <option>{possibility}</option>
-                        {/each}
-                    </select>
-                    <span class="icon is-small is-left">
+    <div class=" column is-2 m-2">
+        <div class="box">
+            <div class="level-item m-3">
+                <p class="subtitle is-size-4 is-italic">Filter users</p>
+            </div>
+            <div class="level-item m-2">
+                <p class="subtitle is-5">Scope</p>
+            </div>
+            <div class="level-item m-2">
+                <div class="control has-icons-left">
+                    <div class="select is-primary">
+                        <select bind:this={userSelect}>
+                            {#each userSelectPossibilities as possibility}
+                                <option>{possibility}</option>
+                            {/each}
+                        </select>
+                        <span class="icon is-small is-left">
                             <i class="fas fa-user-lock"></i>
                         </span>
+                    </div>
                 </div>
             </div>
+            <div class="level-item m-2">
+                <button class="button is-primary is-light " on:click={filterApplied}>Apply filters</button>
+            </div>
         </div>
-        <div class="level-item m-2">
-            <button class="button is-primary is-light " on:click={filterApplied}>Apply filters</button>
+    </div>
+
+    <div class="column is-6 m-2">
+        <div class="box">
+            {#if Object.keys(userFilterList).length === 0}
+                <p><i class="far fa-folder-open"/> <span>No users found for these filters</span></p>
+            {:else}
+                <div class="table-container">
+                    <table class="table is-fullwidth">
+                        <thead>
+                        <th>Name</th>
+                        <th>Scope</th>
+                        </thead>
+                        <tbody>
+                        {#each userFilterList as user}
+                            <tr on:click={clicked(user)}>
+                                <td>
+                                    {user.name}
+                                </td>
+                                <td>
+                                    {#if user.scope === "admin"}
+                                        <span class="tag is-danger is-uppercase">{user.scope}</span>
+                                    {:else }
+                                        <span class="tag is-primary is-uppercase">{user.scope}</span>
+                                    {/if}
+                                </td>
+                            </tr>
+                        {/each}
+                        </tbody>
+                    </table>
+                </div>
+            {/if}
+        </div>
+    </div>
+
+    <div class="column is-3 m-2">
+        <div class="box">
+            <h2 class="title is-6 has-text-centered">Privilege Proportion</h2>
+            <Chart data={privilegeDistributionData} type="pie"/>
+        </div>
+        <div class="box">
+            <h2 class="title is-6 has-text-centered">Average Password Length</h2>
+            <Chart data={averagePwdLength} type="bar"/>
         </div>
     </div>
 </div>
