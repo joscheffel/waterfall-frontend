2b9907012f5d58589fb8c42a223e3d0494312dff
jonathan <jonathan.scheffel@st.oth-regensburg.de>
2022-07-02 06:19:49 -0400

    Some minor improvements to the UI

--- waterfall-front-end/src/pages/Signup.svelte
+++ waterfall-front-end/src/pages/Signup.svelte
@@ -2,6 +2,7 @@
     import TitleBar from "../components/TitleBar.svelte";
     import SignupForm from "../components/SignupForm.svelte";
     import Navigator from "../components/Navigator.svelte";
+    import niagaraFallsNight from "/src/assets/niagaraFallsNight.jpg";
 </script>
 
 <div class="columns is-vcentered">
@@ -20,7 +21,11 @@
             <SignupForm/>
         </div>
     </div>
-    <div class="column has-text-centered">
-        <p>Here could be a teaser picture</p>
+    <div class="column">
+        <div class="box m-2">
+            <h2 class="subtitle is-4 m-2">Niagara Falls by Nightfall</h2>
+            <img src="{niagaraFallsNight}">
+            <span class="tag is-light"><i class="fas fa-camera m-2"></i> by J. Scheffel</span>
+        </div>
     </div>
 </div>
